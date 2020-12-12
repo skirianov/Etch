8879a2e7df2f6ed3393d4d3f9f79ca122dd1bc85
S.Kiri <s.kirianov@gmail.com>
2020-12-12 23:11:36 +0100

    Completed all

--- app.js
+++ app.js
@@ -7,23 +7,24 @@ let div = document.createElement('div');
 const colorPicker = document.querySelector('.color');
 let color = 'red';
 colorPicker.addEventListener('input',() => {
-  
     color = colorPicker.value;
     
 })
 
+// first document onload event
 document.addEventListener('load',onload());
 function onload(){ 
-    for (let i = 0; i < 1000; i++){
-     addDivs(30);
+    for (let i = 0; i < 10000; i++){
+     addDivs(65);
     };
     divs = Array.from(document.getElementsByClassName('div'));
     divs.forEach((each)=> {
         each.addEventListener('mouseover',() => {
-            each.style.backgroundColor = color})});
+            each.style.backgroundColor =  color;
+        })});
 }
 
-function addDivs(n){
+function addDivs(n){ //general add div
         div = document.createElement('div');
         div.style.height = height / n + "px";
         div.style.width = width / n + 'px';
@@ -32,21 +33,43 @@ function addDivs(n){
         container.appendChild(div);
 }
 
+const size = document.querySelector('.range');
+const clear = document.querySelector('.clear');
+const rainbow = document.querySelector('.rainbow');
 
-
-const size = document.querySelector('input').value;
-
-
+// Filling container function
 const fillContainer = () => {
     container.innerHTML = '';
-    for (let i = 0; i < size * size; i++){
-        addDivs(size);
+    for (let i = 0; i < size.value * size.value; i++){
+        addDivs(size.value);
     }
     divs = Array.from(document.getElementsByClassName('div'));
     divs.forEach((each)=> {
         each.addEventListener('mouseover',() => {
-            each.style.backgroundColor = color})})
+            each.style.backgroundColor =  color;
+        })});
 }
 
+// clear button and size change refresh
+clear.addEventListener('click',() => {fillContainer()});
+size.addEventListener('input',()=>{fillContainer()});
+let random = () => {return Math.floor(Math.random() * 255)};
+rainbow.addEventListener('click',() => {
+    divs = Array.from(document.getElementsByClassName('div'));
+    divs.forEach((each)=> {
+        each.addEventListener('mouseover',() => {
+            color = "rgb(" + random() + "," + random() + "," + random() + ")";
+            each.style.backgroundColor =  color;
+        })});
+})
 
+
+
+function paintingDivs(){
+    divs = Array.from(document.getElementsByClassName('div'));
+    divs.forEach((each)=> {
+        each.addEventListener('mouseover',() => {
+            each.style.backgroundColor =  color;
+    })});
+}
 
